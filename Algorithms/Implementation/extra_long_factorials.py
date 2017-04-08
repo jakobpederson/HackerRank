1e21e55c322f9e36207662348e90e1650fcb6cc9
jakobpederson <jakob.pederson+github@gmail.com>
2017-04-08 09:39:38 -0500

    Update extra_long_factorials.py

--- Algorithms/Implementation/extra_long_factorials.py
+++ Algorithms/Implementation/extra_long_factorials.py
@@ -19,6 +19,11 @@ def test_extra_long_factorials():
         print(True)
     else:
         print("False %s" % extra_long_factorials(n))
-    
-#test_extra_long_factorials()
-print(extra_long_factorials(n))
+    m = 88
+    if extra_long_factorials(m) == 185482642257398439114796845645546284380220968949399346684421580986889562184028199319100141244804501828416633516851200000000000000000000:
+        print(True)
+    else:
+        print(False)
+        
+test_extra_long_factorials()
+#print(extra_long_factorials(n))
