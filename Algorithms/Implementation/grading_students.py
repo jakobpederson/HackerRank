c7f41d8f4d896ce4991fd4937fd37cd366a0bbc2
jakobpederson <jakob.pederson+github@gmail.com>
2017-04-15 10:42:13 -0500

    Update grading_students.py

--- Algorithms/Implementation/grading_students.py
+++ Algorithms/Implementation/grading_students.py
@@ -6,20 +6,20 @@ import sys
 def test_grading():
     samples = [73, 67, 38, 33]
     expected = [75, 67, 40, 33]
-    result = list(solve(x) for x in samples)
+    result = solve(samples)
     for grade in expected:
         if grade in result:
             print(True)
         else:
             print("False:", grade)
                         
-def solve(grade):
-    if grade < 38:
-        return grade
-    return custom_rounding_to_multiple_of_five(grade)
+def solve(grades):
+    return list(custom_rounding_to_multiple_of_five(x) for x in grades)
 
 def custom_rounding_to_multiple_of_five(grade):
-    if grade % 5 == 3:
+    if grade < 38:
+        return grade
+    elif grade % 5 == 3:
         grade += 2
     elif grade % 5 == 4:
         grade += 1
@@ -32,7 +32,7 @@ for grades_i in range(n):
     grades_t = int(input().strip())
     grades.append(grades_t)
 
-# test_grading()
-answers = list(solve(x) for x in grades)
+#test_grading()
+answers = solve(grades)
 for answer in answers:
     print(answer)
