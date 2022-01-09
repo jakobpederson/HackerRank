9a4e18f592d3c021eb2da1f26b0fb8b283140e16
jakobpederson <jakob.pederson+github@gmail.com>
2022-01-09 11:35:59 -0600

    wip

--- exercises/non_divisible_subset.py
+++ exercises/non_divisible_subset.py
@@ -4,9 +4,7 @@ from itertools import combinations
 
 
 def get_non_divisible_subset(values, k):
-    counts = Counter()
-    for number in values:
-        counts.update({number % k: 1})
+    counts = get_counts(values)
     counts.update({'answer': min(counts[0], 1)})
     if k % 2 == 0:
         counts.update({'answer': min(counts[k//2], 1)})
@@ -14,6 +12,13 @@ def get_non_divisible_subset(values, k):
         if i != k - i:
             counts.update({'answer': max(counts[i] , counts[k-i])})
     return counts['answer']
+
+
+def get_counts(values, k):
+    counts = Counter()
+    for number in values:
+        counts.update({number % k: 1})
+    return counts
 
 
 
@@ -36,3 +41,12 @@ class NonDivisibleSubset(TestCase):
         k = 3
         result = get_non_divisible_subset(values, k)
         self.assertEqual(result, 3)
+
+    def test_get_counts(self):
+        values = [19, 10, 12, 10, 24, 25, 22]
+        k = 4
+        result = get_counts(values, k)
+        print(f'{result[0]=}')
+        expected = {3: 1, 2: 3, 0: 2, 1: 1}
+        self.assertEqual(dict(result), expected)
+        self.fail('x')
